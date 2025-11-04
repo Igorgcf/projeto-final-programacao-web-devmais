f90cd23e9b09926f4a7cafada2366dd11aaf11c9
Igor Gonçalves <igorTecnologia@outlook.com>
2025-11-04 20:35:21 -0300

    Delete authuser directory

--- authuser/src/main/java/tech/ada/authuser/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package tech.ada.authuser.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String msg){
-        super(msg);
-    }
-}
--- authuser/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_user (id, name, last_name, phone, email, password) VALUES(UUID(), 'John Doe', 'Doe', '11 99999-9999', 'john.doe@gmail.com', '123456');
-
-INSERT INTO tb_user (id, name, last_name, phone, email, password) VALUES(UUID(), 'Jane Doe', 'Doe', '11 88888-8888', 'jane.doe@gmail.com', '123456');
-
-INSERT INTO tb_user (id, name, last_name, phone, email, password) VALUES(UUID(), 'Bruna', 'Haz', '11 77777-7777', 'bruna.haz@gmail.com', '123456');
